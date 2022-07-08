d7c213df30442a4f456dccfaba253ca8a345f40e
rohitkumarpandey <rohitpandey1896@gmail.com>
2022-07-09 01:39:31 +0530

    initial commit

---
+++ src/main/java/webapp/smart/health/auth/api/WebappSmartHealthAuthApiApplication.java
@@ -0,0 +1,13 @@
+package webapp.smart.health.auth.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSmartHealthAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappSmartHealthAuthApiApplication.class, args);
+	}
+
+}
